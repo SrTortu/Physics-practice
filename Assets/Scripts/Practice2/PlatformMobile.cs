0be027e9d6d242f20df69e9dc9668bed86ab45bf
SrTortu <santiagogc0828@gmail.com>
2025-02-28 02:30:51 +0100

    Added practice 2 Practice2: added player, added platform mobile function

---
+++ Assets/Scripts/Practice2/PlatformMobile.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class PlatformMobile : MonoBehaviour
+{
+    #region Enum
+
+    public enum MoveDirections
+    {
+        Right,
+        Left
+    }
+
+    #endregion
+    #region Fields
+
+    private const float MOVE_DISTANCE = 10f;
+
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private MoveDirections _moveDirection;
+
+    private Rigidbody _platformRigidbody;
+    private Vector3 _pointLeft;
+    private Vector3 _pointRight;
+    private Vector3 _targetPoint;
+
+    #endregion
+
+    #region UnityCallbacks
+
+    private void Awake()
+    {
+        _platformRigidbody = GetComponent<Rigidbody>();
+        _pointLeft = new Vector3(this.transform.position.x - MOVE_DISTANCE, this.transform.position.y,
+            this.transform.position.z);
+        _pointRight = new Vector3(this.transform.position.x + MOVE_DISTANCE, this.transform.position.y,
+            this.transform.position.z);
+        _targetPoint = _moveDirection == MoveDirections.Right ? _pointRight : _pointLeft;
+    }
+
+    private void FixedUpdate()
+    {
+        _platformRigidbody.MovePosition(Vector3.MoveTowards(transform.position, _targetPoint, _moveSpeed)); 
+        if (Vector3.Distance(transform.position, _targetPoint) < 0.1f)
+        {
+            _targetPoint = _targetPoint == _pointRight ? _pointLeft : _pointRight;
+        }
+    }
+
+    #endregion
+}
